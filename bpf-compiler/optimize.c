071231f13567bad0387c36a2c6cfad7faf15ca50
bl0ckeduser <bl0ckedusersoft@gmail.com>
2012-12-19 17:50:15 -0500

    Improve binary-tree simplification

--- bpf-compiler/optimize.c
+++ bpf-compiler/optimize.c
@@ -9,21 +9,22 @@ void optimize(exp_tree_t *et)
 	exp_tree_t *below;
 	int all_nums;
 
+	for (i = 0; i < et->child_count; i++) {
+		optimize(et->child[i]);
+	}
+
 	/* Simplify nested binary trees.
 	 * e.g. (MULT (NUMBER:2) (MULT (NUMBER:3) (NUMBER:4)))
 	 * becomes (MULT (NUMBER:2) (NUMBER:3) (NUMBER:4))
 	 */
 	if (et->child_count == 2 
-		&& et->child[1]->child_count == 2
 		&& et->child[1]->head_type == et->head_type)
 	{
 		below = et->child[1];
 		et->child_count = 1;
-		add_child(et, below->child[0]);
-		add_child(et, below->child[1]);
+		for (i = 0; i < below->child_count; i++) {
+			add_child(et, below->child[i]);
+		}
 	}
 
-	for (i = 0; i < et->child_count; i++) {
-		optimize(et->child[i]);
-	}
 }
