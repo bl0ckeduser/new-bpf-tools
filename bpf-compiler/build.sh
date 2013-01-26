2ccdfab0597a419eba53dd66cc73b14881ebcfd3
bl0ckeduser <bl0ckedusersoft@gmail.com>
2013-01-26 12:40:36 -0500

    Rename VM codegen file more appropriately

--- bpf-compiler/build.sh
+++ bpf-compiler/build.sh
@@ -1,1 +1,1 @@
-c99 codegen.c diagnostics.c general.c main.c optimize.c parser.c tokenizer.c tokens.c tree.c
+c99 codegen_bpf.c diagnostics.c general.c main.c optimize.c parser.c tokenizer.c tokens.c tree.c
