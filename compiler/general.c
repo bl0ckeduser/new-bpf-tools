1985ce0707275530ab1c5de795e6d1e542839868
bl0ckeduser <bl0ckedusersoft@gmail.com>
2013-08-22 14:26:43 -0400

    general: add findtok() -- DFS for a token in an tree

--- compiler/general.c
+++ compiler/general.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "tree.h"
 
 void fail(char* mesg)
 {
@@ -13,4 +14,16 @@ void sanity_requires(int exp)
 {
 	if(!exp)
 		fail("that doesn't make any sense");
+}
+
+token_t *findtok(exp_tree_t *et)
+{
+	int i;
+	token_t *t;
+	if (et->tok)
+		return et->tok;
+	for (i = 0; i < et->child_count; ++i)
+		if ((t = findtok(et->child[i])))
+			return t;
+	return NULL;
 }
