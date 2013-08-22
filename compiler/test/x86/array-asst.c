618af1f215b31252e205d35b7ecd1f434e6b46ec
bl0ckeduser <bl0ckedusersoft@gmail.com>
2013-08-21 22:50:49 -0400

    x86: add some documentation on newer features

--- compiler/test/x86/array-asst.c
+++ compiler/test/x86/array-asst.c
@@ -7,23 +7,34 @@ main()
 
 	for (i = 0; i < 7 * 10; ++i)
 		if (i % 7 == 6) {
+			/*
+			 * the compiler correctly writes
+			 * into char arrays
+			 */
 			buf[i] = ' ';
+
+			/* and into int arrays */
 			derp[i] = buf[i];
 		}
 		else {
+			/* random amusing pattern ... */
 			buf[i] = 'a' + (i % 7);
 			derp[i] = buf[i];
 		}
-	derp[i] = buf[i] = 0;
+	derp[i] = buf[i] = 0;	// null-termination
 
+	/* test the char array writing */
 	puts(buf);
 	for (i = 0; derp[i]; ++i)
 		putchar(derp[i]);
 	printf("\n");
 
+	/* test the int array writing */
 	for (i = 0; buf[i]; ++i)
 		putchar(buf[i]);
 	printf("\n");
+
+	/* check the sizes of int and char */
 
 	ptr1 = &buf[5];
 	ptr2 = &buf[0];
