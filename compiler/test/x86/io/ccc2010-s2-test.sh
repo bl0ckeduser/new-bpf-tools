00f540511bdb5965a29d7ce3a2a7209a9327c9aa
bl0ckeduser <bl0ckedusersoft@gmail.com>
2013-08-31 23:24:31 -0400

    Add CCC 2010 Huffman encoding test code

---
+++ compiler/test/x86/io/ccc2010-s2-test.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# 	This script  must be run from its directory.
+#	Warning: waterloo's UNIX_OR_MAC.zip file is ?? MB in size.
+#	Uncomment the below two lines if you don't have the .zip file yet
+#	and/or have not unpacked it yet
+
+#wget http://cemc.uwaterloo.ca/contests/computing/2010/stage1/Stage1Data/UNIX_OR_MAC.zip
+#unzip UNIX_OR_MAC.zip
+
+cp ../../../compile-run-x86.sh .
+cp ../../../a.out .
+
+ptot=0
+ftot=0
+for prob in s2
+do
+	pass=0
+	fail=0
+	for cas in `ls UNIX_OR_MAC/S2/*.in`
+	do
+		chkfile=`echo $cas | sed 's/\.in/\.out/'`
+		out=`cat $cas | (timeout -k 1 1 ./compile-run-x86.sh ccc2010-s2.c 2>/dev/null) | sum`
+		chk=`cat $chkfile | sum`
+		if [ "$chk" = "$out" ]
+		then
+			echo ${cas}: PASS
+		else
+			echo ${cas}: FAIL
+		fi
+	done
+done
---
+++ compiler/test/x86/io/ccc2010-s2.c
@@ -0,0 +1,97 @@
+/*
+ * This is a solution to the problem "Problem S2: Huffman Encoding"
+ * which was given at the first stage of the 2010 edition of the
+ * Canadian Computing Competition. I wrote this code on Feb. 15, 2013
+ * while practicing for the 2013 edition of the competiton.
+ * (The changes from the original C version, other than removing
+ * standard library preprocessor #includes, are marked with XXX).
+ *
+ * The problem description is given in:
+ * http://cemc.uwaterloo.ca/contests/computing/2010/stage1/seniorEn.pdf
+ *
+ * And Waterloo provides test cases at:
+ * http://cemc.uwaterloo.ca/contests/computing/2010/stage1/Stage1Data/UNIX_OR_MAC.zip
+ *
+ * I wrote a script to try the test cases, it's called "ccc2010-s2-test.sh";
+ * it's in this directory, it has to be run from this directory, and it needs
+ * some tweaking before it actually works.
+ */
+
+typedef struct node {
+	struct node* l;
+	struct node* r;
+	char val;
+	int has_val;
+} node_t;
+
+node_t* new_node() {
+	node_t* n = malloc(sizeof(node_t));
+	if (!n) {
+		printf("memory failure\n");
+		exit(1);
+	}
+	n->has_val = 0;
+	n->l = n->r = /* XXX: NULL */ 0x0;
+	return n;
+}
+
+main()
+{
+	int i;
+	int n;
+	char buf[1024];
+	char patt[1024];
+	char c;
+	char *p;
+	node_t *root = new_node();
+	node_t *nod;
+
+	/* XXX: fgets(buf, 1024, stdin); */
+	gets(buf);
+	sscanf(buf, "%d", &n);
+
+	for (i = 0; i < n; ++i) {
+		/* XXX: fgets(buf, 1024, stdin); */
+		gets(buf);
+		sscanf(buf, "%c %s", &c, patt);
+		/* XXX: for (p = patt, nod = root; *p; ++p) { */
+		p = patt;
+		for (nod = root; *p; ++p) {
+			if (*p == '0') {
+				if (!nod->l)
+					nod->l = new_node();
+				nod = nod->l;
+			} else if (*p == '1') {
+				if (!nod->r)
+					nod->r = new_node();
+				nod = nod->r;
+			}
+		}
+		nod->has_val = 1;
+		nod->val = c;
+	}
+
+	/* XXX: fgets(patt, 1024, stdin); */
+	gets(patt);
+	p = patt;
+	for (nod = root; *p; ++p) {
+	/* XXX: for (p = patt, nod = root; *p; ++p) { */
+		if (*p == '0') {
+			if (nod->l)
+				nod = nod->l;
+			else
+				nod = root;
+		} else if(*p == '1') {
+			if (nod->r)
+				nod = nod->r;
+			else
+				nod = root;
+		}
+		if (nod->has_val) {
+			putchar(nod->val);
+			nod = root;
+		}
+	}
+	/* XXX: putchar('\n'); */
+	printf("\n");
+}
