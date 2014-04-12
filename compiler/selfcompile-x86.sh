673f11345387fa04e931cb057d9fed75a0f45906
bl0ckeduser <bl0ckedusersoft@gmail.com>
2014-04-12 14:54:13 -0400

    add selfcompliation script (not fully working yet)

---
+++ compiler/selfcompile-x86.sh
@@ -0,0 +1,8 @@
+if ! test -e wcc
+then
+	echo "please compile the standalone wcc command via ./build-wcc.sh"
+	exit 1
+fi
+
+./wcc -g -DDEBUG -o wcc2 tokenizer.c general.c tokens.c hashtable.c diagnostics.c main.c optimize.c tree.c preprocess.c escapes.c typeof.c codegen_x86.c parser.c
+
