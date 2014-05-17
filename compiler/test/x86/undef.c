24b440061ee59f2a24b29bddc85aaf02d2ba94c6
bl0ckeduser <bl0ckedusersoft@gmail.com>
2014-05-17 10:24:05 -0400

    test case for #undef

---
+++ compiler/test/x86/undef.c
@@ -0,0 +1,24 @@
+main()
+{
+	#define DOUG "hahaha"
+	#define BOB 123
+	#undef BOB
+	#define BOB 456
+
+/*
+ * XXX: this should work --
+ * the preprocessor should substitue in the BOB
+ * before the undef occurs, which currently it doesn't
+ */
+/*
+	printf("BOB=%d\n", BOB);
+*/
+	
+	puts(DOUG);
+
+	#undef BOB
+
+	#ifdef BOB
+		printf("wrong, wrong, wrong\n");
+	#endif
+}
