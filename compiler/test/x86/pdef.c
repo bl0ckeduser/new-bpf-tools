d53624d51694dc7bf3e5259aa4086d9fdd6c59d0
bl0ckeduser <bl0ckedusersoft@gmail.com>
2014-05-19 10:15:57 -0400

    more tests for parameterized #defines

--- compiler/test/x86/pdef.c
+++ compiler/test/x86/pdef.c
@@ -3,10 +3,18 @@
  */
 
 #define bob(x,y) x+y
+#define doug(a,b) a*b
+#define crappy_max(a,b) ((a) > (b) ? (a) : (b))
 
 main()
 {
+	printf("%d\n", crappy_max(100, 200));
+	printf("%d\n", crappy_max(200, 200));
+	printf("%d\n", crappy_max(200, 100));
+	printf("%d\n", crappy_max(100, 100));
+
 	/* recursive substiution support is important */
+	printf("%d\n", doug(bob(123,456), doug(123, 456)));
 	printf("%d\n", bob(123,456));
 	printf("%d\n", bob( bob(123,456), bob(123,456) ));
 	printf("%d\n", bob( bob(bob(123,456),456), bob(123,456) ));
