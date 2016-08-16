91f99780583b02d6192870e99e2c6982a41215a4
bl0ckeduser <bl0ckedusersoft@gmail.com>
2016-08-16 17:19:32 -0400

    Fix indent of euler33.c test

--- compiler/test/x86/euler33.c
+++ compiler/test/x86/euler33.c
@@ -10,117 +10,117 @@
 
 void main(void)
 {
-int test;
-int x, keycount, y, num_digit1;
-int num_digit2, denom_digit1, denom_digit2;
-int test2, c;
-int num_keys[100];
-int denom_keys[100];
-int factor_count[1000];
-int product;
-int denom;
-int tp;
-
-int numFactors[10000];
-int denomFactors[10000];
-
-
-x = 10;
+	int test;
+	int x, keycount, y, num_digit1;
+	int num_digit2, denom_digit1, denom_digit2;
+	int test2, c;
+	int num_keys[100];
+	int denom_keys[100];
+	int factor_count[1000];
+	int product;
+	int denom;
+	int tp;
+	
+	int numFactors[10000];
+	int denomFactors[10000];
 
 
+	x = 10;
 
-keycount = 0;
 
-while(x<100)
-{
-	y = 10;
-	while(y<x)
+
+	keycount = 0;
+	
+	while(x<100)
 	{
-		if((x%10!=0)*(y%10!=0))
+		y = 10;
+		while(y<x)
 		{
-
-			num_digit1 = (y/10);
-			num_digit2 = y-(10*(y/10));
+			if((x%10!=0)*(y%10!=0))
+			{
+	
+				num_digit1 = (y/10);
+				num_digit2 = y-(10*(y/10));
+				
+				denom_digit1 = (x/10);
+				denom_digit2 = x-(10*(x/10));
 			
-			denom_digit1 = (x/10);
-			denom_digit2 = x-(10*(x/10));
-		
-			if(num_digit1==denom_digit1)
-			{
-				test = y*1000/x;
-				test2 = num_digit2*1000/denom_digit2;
-				if(test==test2)
+				if(num_digit1==denom_digit1)
 				{
-					keycount++;
-					num_keys[keycount] = y;
-					denom_keys[keycount] = x;
+					test = y*1000/x;
+					test2 = num_digit2*1000/denom_digit2;
+					if(test==test2)
+					{
+						keycount++;
+						num_keys[keycount] = y;
+						denom_keys[keycount] = x;
+					}
+				}	
+	
+				if(num_digit1==denom_digit2)
+				{
+					test = y*1000/x;
+					test2 = num_digit2*1000/denom_digit1;
+					if(test==test2)
+					{
+						keycount++;
+						num_keys[keycount] = y;
+						denom_keys[keycount] = x;
+					}
 				}
-			}
-
-			if(num_digit1==denom_digit2)
-			{
-				test = y*1000/x;
-				test2 = num_digit2*1000/denom_digit1;
-				if(test==test2)
+		
+				if(num_digit2==denom_digit1)
 				{
-					keycount++;
-					num_keys[keycount] = y;
-					denom_keys[keycount] = x;
+					test = y*1000/x;
+					test2 = num_digit1*1000/denom_digit2;
+					if(test==test2)
+					{
+						keycount++;
+						num_keys[keycount] = y;
+						denom_keys[keycount] = x;
+					}
+				}
+	
+				if(num_digit2==denom_digit2)
+				{
+					test = y*1000/x;
+					test2 = num_digit1*1000/denom_digit1;
+					if(test==test2)
+					{
+						keycount++;
+						num_keys[keycount] = y;
+						denom_keys[keycount] = x;
+					}
 				}
 			}
 	
-			if(num_digit2==denom_digit1)
-			{
-				test = y*1000/x;
-				test2 = num_digit1*1000/denom_digit2;
-				if(test==test2)
-				{
-					keycount++;
-					num_keys[keycount] = y;
-					denom_keys[keycount] = x;
-				}
-			}
-
-			if(num_digit2==denom_digit2)
-			{
-				test = y*1000/x;
-				test2 = num_digit1*1000/denom_digit1;
-				if(test==test2)
-				{
-					keycount++;
-					num_keys[keycount] = y;
-					denom_keys[keycount] = x;
-				}
-			}
+			y++;
 		}
-
-		y++;
+		x++;
 	}
-	x++;
-}
-
-c = 0;
-denom = 1;
-product = 1;
-while(c++<keycount)
-{
-	printf("%d / %d\n", num_keys[c], denom_keys[c]);
-	denom*=denom_keys[c];
-	product*=num_keys[c];
-}
-
-printf("Product (not simplified): %d / %d \n", product, denom);
-
-tp = 1;
-while(tp++<denom)
-{
-	while(denom%tp==0 && product%tp==0)
+	
+	c = 0;
+	denom = 1;
+	product = 1;
+	while(c++<keycount)
 	{
-		product/=tp;	
-		denom/=tp;
-		printf("Product: %d / %d - %d\n", product, denom, tp);
+		printf("%d / %d\n", num_keys[c], denom_keys[c]);
+		denom*=denom_keys[c];
+		product*=num_keys[c];
+	}
+	
+	printf("Product (not simplified): %d / %d \n", product, denom);
+	
+	tp = 1;
+	while(tp++<denom)
+	{
+		while(denom%tp==0 && product%tp==0)
+		{
+			product/=tp;	
+			denom/=tp;
+			printf("Product: %d / %d - %d\n", product, denom, tp);
+		}
 	}
 }
 
-
-}
+		
